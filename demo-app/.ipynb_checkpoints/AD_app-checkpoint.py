f0bbec925691215c51009e08e61d99c9efd3a072
ferastwal <ferastwal2@gmail.com>
2020-11-17 23:56:15 -0600

    new best performing model

--- demo-app/.ipynb_checkpoints/AD_app-checkpoint.py
+++ demo-app/.ipynb_checkpoints/AD_app-checkpoint.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import tensorflow as tf
 from PIL import Image
 from tensorflow import keras
 from tensorflow.keras.models import load_model
@@ -8,7 +9,7 @@ from skimage.transform import resize
 
 
 st.title("Alzheimer's Disease Classifier")
-st.header("Classifies brain MRI images as AD or normal")
+st.header("Detects AD using brain MRI images")
 st.header("")
 
 uploaded_img = st.file_uploader('Choose an MRI image', type=['png', 'jpg', 'jpeg'])
@@ -17,12 +18,12 @@ if uploaded_img is not None:
     img_array = np.asarray(my_image)
     new_array = resize(img_array, (150, 150, 3))
     new_array = new_array.reshape(-1, 150, 150, 3)
-    st.image(new_array, caption='Uploaded MRI image.', use_column_width=True)
+    st.image(my_image, caption='Uploaded MRI image.', use_column_width=True)
     # new_array = (new_array.reshape(-1, 150, 150, 3))
 
 
 #loading the model
-AD_classifier = load_model('/Users/ferasaltwal/Documents/DSI/New-Capstone/saved-models/Kaggle-jupyter.h5')
+AD_classifier = load_model('/Users/ferasaltwal/Documents/DSI/New-Capstone/saved-models/BN-model-TL.h5')
 
 #write a function to classify the input image
 # def predict_input(classifier_model, input):
@@ -40,14 +41,14 @@ if st.button("Click here to classify"):
     else:
         
         with st.spinner('Classifying ...'):
-            prediction = AD_classifier.predict(new_array)
+            prediction = AD_classifier.predict_classes(new_array)
             st.success('Done!')
         st.header('Model predicts: ')
 
 
-        if prediction <= 0.5:
+        if prediction == 1:
             
-            st.write("Alzheimer's Disease", '\n' )
+            st.write("Alzheimer's Disease", '\n', )
             
             
                              
